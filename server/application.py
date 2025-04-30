705f667afa9d421aa664aeb070ab80938e145dd6
SteepDay <steepday@gmail.com>
2025-04-30 20:05:00 +0500

    исправил класс

--- server/application.py
+++ server/application.py
@@ -1,11 +1,11 @@
 import http.server
 import socketserver
-PORT = 8000
-class TestMe():
+
+class TestMe:
     def take_five(self):
         return 5
     def port(self):
-        return PORT
+        return 8000
 if __name__ == '__main__':
     Handler = http.server.SimpleHTTPRequestHandler
     with socketserver.TCPServer(("", PORT), Handler) as http:
