e5d9e4edc7c5357c0cb78feb23dafa6d2f0fefe8
SteepDay <steepday@gmail.com>
2025-04-30 20:08:42 +0500

    исправил класс

--- server/application.py
+++ server/application.py
@@ -8,6 +8,7 @@ class TestMe:
         return 8000
 if __name__ == '__main__':
     Handler = http.server.SimpleHTTPRequestHandler
+    PORT = 8000
     with socketserver.TCPServer(("", PORT), Handler) as http:
         print("serving at port", PORT)
         http.serve_forever()
